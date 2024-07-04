a1d902262d1860aa2e1e97ce5f4104df48065e69
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-07-04 12:41:16 +0530

    merge

--- src/Test.js
+++ src/Test.js
@@ -2,17 +2,34 @@ import React, { useEffect } from 'react'
 
 export default function Test() {
 
-
    useEffect(() => {
-      const cube = document.querySelector('.cube');
-      if(cube){
-         cube.style.transform = 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
-      }
+      const cubes = document.getElementsByClassName('cube');
+      Array.from(cubes).forEach(cube => {
+         cube.style.transition = 'transform 1s';  
+         cube.style.transform = 'rotateX(0deg) rotateY(90deg) rotateZ(0deg)';
+       });
    }, []);
 
   return (
     <div className='bg-black min-h-screen flex justify-center items-center'>
-      <div className="container">
+      <div>
+         <div className="flex">
+          <div className="cube" >
+            <div className="wall text-white front">Front</div>
+            <div className="wall text-white back">Back</div>
+            <div className="wall text-white left">Left</div>
+            <div className="wall text-white right">Right</div>
+            <div className="wall text-white top">Top</div>
+            <div className="wall text-white bottom">Bottom</div>
+          </div>
+          <div className="cube">
+            <div className="wall text-white front"></div>
+            <div className="wall text-white back">Back</div>
+            <div className="wall text-white left">Left</div>
+            <div className="wall text-white right"></div>
+            <div className="wall text-white top">Top</div>
+            <div className="wall text-white bottom">Bottom</div>
+          </div>
           <div className="cube">
             <div className="wall text-white front">Front</div>
             <div className="wall text-white back">Back</div>
@@ -22,6 +39,35 @@ export default function Test() {
             <div className="wall text-white bottom">Bottom</div>
           </div>
         </div>
+         <div className="flex">
+          <div className="cube">
+            <div className="wall text-white front"></div>
+            <div className="wall text-white back">Back</div>
+            <div className="wall text-white left">Left</div>
+            <div className="wall text-white right"></div>
+            <div className="wall text-white top">Top</div>
+            <div className="wall text-white bottom">Bottom</div>
+          </div>
+          <div className="cube">
+            <div className="wall text-white front">Front</div>
+            <div className="wall text-white back">Back</div>
+            <div className="wall text-white left">Left</div>
+            <div className="wall text-white right">Right</div>
+            <div className="wall text-white top">Top</div>
+            <div className="wall text-white bottom">Bottom</div>
+          </div>
+          <div className="cube">
+            <div className="wall text-white front"></div>
+            <div className="wall text-white back">Back</div>
+            <div className="wall text-white left">Left</div>
+            <div className="wall text-white right"></div>
+            <div className="wall text-white top">Top</div>
+            <div className="wall text-white bottom">Bottom</div>
+          </div>
+        </div>
+         
+
+      </div>
     </div>
   )
 }
