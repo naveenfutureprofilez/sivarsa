3c77c92091ba145937d9ba5910e91be68fa3c2e3
Abhinav-FP <a.mathur@futureprofilez.com>
2024-07-04 11:50:36 +0530

    Created Branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Page from './home/Page';
+import Page2 from './home/Page2';
 
 function App() {
   return (
     <Router basename="/">
      <Routes>
      <Route path="/" element={<Page />} />
+     <Route path="/hello" element={<Page2 />} />
      </Routes>
   </Router>
   );
