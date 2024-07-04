fcbfd3d41da894319953e6081a36d20ca73c948c
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-07-04 12:11:41 +0530

    merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import './test.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Page from './home/Page';
+import Test from './Test';
 
 function App() {
   return (
     <Router basename="/">
      <Routes>
      <Route path="/" element={<Page />} />
+     <Route path="/test" element={<Test />} />
      </Routes>
   </Router>
   );
---
+++ src/Test.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+export default function Test() {
+
+
+   useEffect(() => {
+      const cube = document.querySelector('.cube');
+      cube.style.transform = 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
+   }, []);
+
+  return (
+    <div className='bg-black min-h-screen flex justify-center items-center'>
+      <div className="container">
+        <div className="flex">
+          <div className="cube">
+            <div className="wall text-white front">Front</div>
+            <div className="wall text-white back">Back</div>
+            <div className="wall text-white left">Left</div>
+            <div className="wall text-white right">Right</div>
+            <div className="wall text-white top">Top</div>
+            <div className="wall text-white bottom">Bottom</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
