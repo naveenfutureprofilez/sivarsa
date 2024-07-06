50f5d9cd60e85d7f04ad1c47dedb1e0001b22334
fpmukesh <mukesh@fpdemo.com>
2024-07-06 14:40:43 +0530

    screen7

--- src/home/Landing.js
+++ src/home/Landing.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { arrowblackRT, arrowblackDW,arrowblackUP,weavingSoft,PreBg,nextBg,sivarsaLogo,messageUs } from "../Icons";
-import messageus from "../images/messageus.png"
+import { arrowblackRT, arrowblackDW,arrowblackUP,weavingSoft,PreBg,nextBg,sivarsaLogo,messageUs,ourJourney } from "../Icons";
+import oedgegrid from "../images/oedgegrid.svg"
+import gavs from "../images/gavs.svg"
+import hestus from "../images/hestus.svg"
+import congn from "../images/congn.svg"
+import quoteicon from "../images/quoteicon.png"
+import rabit from "../images/rabit.png"
+import founderbg from "../images/founderbg.svg"
+import boyicon from "../images/boyicon.png"
+import menicon from "../images/menicon.png"
 import { Link } from "react-router-dom";
 export default function Landing() {
 
@@ -14,7 +22,7 @@ export default function Landing() {
 
   return (
     <div className="grid w-full h-screen gap-1 grid-cols-3 grid-rows-custome bg-black">
-      <div className="thumbnailBox h-full"></div>
+      {/* <div className="thumbnailBox h-full"></div>
 
       <div className={`${rotate ? "rotate" : ""} thumbnailBox h-full relative `}>
           <div className="cube h-full relative bg-[#545454]" >
@@ -25,13 +33,12 @@ export default function Landing() {
               <div className="wall text-white left  bg-[#545454] p-2 sm:p-3 md:p-4 lg:p-6">
               Left
                </div>
-
+               
               <div className="wall text-white right">Right</div>
               <div className="wall text-white top">Top</div>
               <div className="wall text-white bottom">Bottom</div>
           </div>
       </div>
-
 
       <div className="thumbnailBox h-full bg-[#171717] "></div>
 
@@ -42,7 +49,7 @@ export default function Landing() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left  bg-[#393939] p-2 sm:p-3 md:p-4 lg:p-6">
-              <h2 className="font-GoshaSans text-xm  md:text-2xl lg:text-4xl xl:text-5xl text-white  ">Services <br /> Provided</h2>
+              <h2 className="font-neuebit leading-normal text-xm  md:text-2xl lg:text-6xl xl:text-7xl leading-none text-white  ">Services <br /> Provided</h2>
               </div>
               <div className="wall text-white right"></div>
               <div className="wall text-white top"></div>
@@ -57,8 +64,8 @@ export default function Landing() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left p-2 sm:p-3 md:p-4 lg:p-6 bg-[#FFC700]">
-               <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl text-[#9D5209] mb-2 sm:mb-3 md:mb-4 lg:mb-5">Staffing</h2>
-               <p className="font-GoshaSans text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl text-[#9D5209]">We provide skilled <br /> professionals across <br /> various domains</p>
+               <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#9D5209] mb-2 sm:mb-3 md:mb-4 lg:mb-5">Staffing</h2>
+               <p className="font-GoshaSans text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#9D5209]">We provide skilled <br /> professionals across <br /> various domains</p>
                <div className="absolute bottom-2 xm:bottom-4 md:bottom-5 lg:bottom-7 xl:bottom-8 right-2 sm:right-4 md:right-5 lg:right-7 xl:right-8" dangerouslySetInnerHTML={{ __html: arrowblackRT }}></div>
               </div>
               <div className="wall text-white right"></div>
@@ -74,8 +81,8 @@ export default function Landing() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left p-2 sm:p-3 md:p-4 lg:p-6 bg-[#2D68FF]">       
-          <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl text-[#A9D6DD] mb-5">Mobile Application</h2>
-        <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl text-[#A9D6DD]">
+          <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#A9D6DD] mb-5">Mobile Application</h2>
+        <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#A9D6DD]">
           We can Craft powerful and <br /> dynamic solutions
         </p>
         <div
@@ -88,7 +95,272 @@ export default function Landing() {
               <div className="wall text-white top"></div>
               <div className="wall text-white bottom"></div>
           </div>
-      </div>
+      </div> */}
+
+
+
+
+
+
+{/* Next Screen 4 */}
+
+  <div className="thumbnailBox h-full bg-[#171717] "></div>
+
+  <div className="thumbnailBox h-full bg-[#171717] "></div>
+
+  <div className="thumbnailBox h-full bg-[#171717] relative ">
+    <div className="block h-full journeyimg " dangerouslySetInnerHTML={{ __html: ourJourney }}></div>
+    <div className="z-10  absolute bottom-1 left-6  font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-6xl leading-1 md:leading-1 lg:leading-9 xl:leading-none text-[#F5F5F5] mb-5">
+    Our <br/> Journey
+    </div>
+  
+  </div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] "></div>
+
+<div className="thumbnailBox h-full bg-[#6FF04A]  p-2 sm:p-3 md:p-4 lg:p-5">
+  <h2 className="relative font-neuebit font-size-200 leading-none text-[#0B6222] leading-normal mb-1">1000 <span className="absolute top-0 sm:top-1 md:top-5 lg:top-6 xl:top-7 text-xl md:text-3xl lg:text-4xl xl:text-5xl">+</span></h2>
+  <h3 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-3xl leading-none text-[#0B6222] mb-5">Coffees consumed</h3>
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] "></div>
+
+
+
+<div className="thumbnailBox h-full bg-[#763DF8]  p-2 sm:p-3 md:p-4 lg:p-5">
+  <h2 className="relative font-neuebit font-size-200 leading-none text-[#CFB8E5] leading-normal mb-1">9 <span className="absolute top-0 sm:top-1 md:top-5 lg:top-6 xl:top-7 text-xl md:text-3xl lg:text-4xl xl:text-5xl">+</span></h2>
+  <h3 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-3xl leading-none text-[#CFB8E5] mb-5">Projects Delivered</h3>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#C7BDFA]  p-2 sm:p-3 md:p-4 lg:p-5">
+  <h2 className="relative font-neuebit font-size-200 leading-none text-[#763DF8] leading-normal mb-1">1k <span className="absolute top-0 sm:top-1 md:top-5 lg:top-6 xl:top-7 text-xl md:text-3xl lg:text-4xl xl:text-5xl">+</span></h2>
+  <h3 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-3xl leading-none text-[#763DF8] mb-5">recruits</h3>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#545454]  p-2 sm:p-3 md:p-4 lg:p-5">
+  <h2 className="relative font-neuebit font-size-200 leading-none text-[#F5F5F5] leading-normal mb-1">15 <span className="absolute top-0 sm:top-1 md:top-5 lg:top-6 xl:top-7 text-xl md:text-3xl lg:text-4xl xl:text-5xl">+</span></h2>
+  <h3 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-3xl leading-none text-[#F5F5F5] mb-5">Coffees consumed</h3>
+</div>
+
+
+{/* end */}
+
+
+
+
+
+
+{/* Next Screen 15 */}
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+  <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#CFB8E5] mb-5">Latest <br/> Work</h2>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+  <div className="h-full flex items-center justify-center">
+    <img src={oedgegrid} alt="img"/>
+  </div>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+  <div className="h-full flex items-center justify-center">
+    <img src={gavs} alt="img"/>
+  </div>
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+  <div className="h-full flex items-center justify-center">
+    <img src={hestus} alt="img"/>
+  </div>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+  <div className="h-full flex items-center justify-center">
+    <img src={congn} alt="img"/>
+  </div>
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+{/* end */}
+
+
+
+{/* Next Screen 6 */}
+<div className="relative thumbnailBox h-full bg-[#F5F5F5] p-2 sm:p-3 md:p-4 lg:p-6">
+    <h2 className="font-neuebit  text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#171717] mb-5">Client <br/> words</h2>
+  <div className="absolute bottom-1 right-1">
+      <img src={rabit} alt="img" />
+  </div>
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717]  p-2 sm:p-3 md:p-4 lg:p-6">
+  <div className=" relative ">
+    <div className="position top-0 left-0">
+      <img src={quoteicon} alt="img"/>
+    </div>
+    <div className="ps-0 md:ps-3 lg:ps-4 xl:ps-5">
+  <p className="font-GoshaSans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5] mb-2">Their AI and design <br/> solutions transformed our <br/> business. Highly <br/> recommended!</p>
+  <h3 className="font-italianno  italic text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5]">Hestus</h3>
+  </div>
+  </div>
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717]  p-2 sm:p-3 md:p-4 lg:p-6">
+  <div className=" relative ">
+    <div className="position top-0 left-0">
+      <img src={quoteicon} alt="img"/>
+    </div>
+    <div className="ps-0 md:ps-3 lg:ps-4 xl:ps-5">
+  <p className="font-GoshaSans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5] mb-2">Unmatched tech and design <br/> expertise. Our go-to firm!</p>
+  <h3 className="font-italianno  italic text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5]">Petco</h3>
+  </div>
+  </div>
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717]  p-2 sm:p-3 md:p-4 lg:p-6">
+  <div className=" relative ">
+    <div className="position top-0 left-0">
+      <img src={quoteicon} alt="img"/>
+    </div>
+    <div className="ps-0 md:ps-3 lg:ps-4 xl:ps-5">
+  <p className="font-GoshaSans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5] mb-2">Exceptional AI solutions and <br/> innovative designs. <br/> Exceeded expectations!</p>
+  <h3 className="font-italianno  italic text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5]">Samatha T.</h3>
+  </div>
+  </div>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717]  p-2 sm:p-3 md:p-4 lg:p-6">
+  <div className=" relative ">
+    <div className="position top-0 left-0">
+      <img src={quoteicon} alt="img"/>
+    </div>
+    <div className="ps-0 md:ps-3 lg:ps-4 xl:ps-5">
+  <p className="font-GoshaSans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5] mb-2">Professional, responsive, <br/> and creative. Perfect blend <br/> of tech and design!</p>
+  <h3 className="font-italianno  italic text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5]">James K</h3>
+  </div>
+  </div>
+</div>
+
+
+
+{/* end */}
+
+
+
+{/* Next Screen 7 */}
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#f5f5f5] ">
+<h2 className="absolute font-neuebit  text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-6xl leading-none text-[#F5F5F5] mb-5">Founders</h2>
+ <div className="h-full founderbg">
+  <img src={founderbg} alt="img" />
+ </div>
+
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="relative thumbnailBox h-full bg-[#2D68FF]  p-2 sm:p-3 md:p-4 lg:p-6">
+  <div className=" relative ">
+    <div className="position top-0 left-0">
+      <img src={quoteicon} alt="img"/>
+    </div>
+    <div className="ps-0 md:ps-3 lg:ps-4 xl:ps-5">
+  <p className="font-GoshaSans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5] mb-2">Transforming ideas into seamless <br/> software experiences</p>
+  <h3 className="font-italianno  italic text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5]">Rishab</h3>
+  </div>
+  </div>
+  <div className="absolute bottom-0 right-0">
+      <img src={boyicon} alt="img"/>
+    </div>
+</div>
+
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+<div className="thumbnailBox h-full bg-[#171717] ">
+
+</div>
+
+
+<div className="relative thumbnailBox h-full bg-[#763DF8]  p-2 sm:p-3 md:p-4 lg:p-6">
+  <div className=" relative ">
+    <div className="position top-0 left-0">
+      <img src={quoteicon} alt="img"/>
+    </div>
+    <div className="ps-0 md:ps-3 lg:ps-4 xl:ps-5">
+  <p className="font-GoshaSans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5] mb-2">Unmatched tech and <br/> design expertise. Our <br/> go-to firm!</p>
+  <h3 className="font-italianno  italic text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F5F5F5]">Rishab</h3>
+  </div>
+  </div>
+  <div className="absolute bottom-0 right-0">
+      <img src={menicon} alt="img"/>
+    </div>
+</div>
+
+{/* end */}
 
      
 
@@ -103,13 +375,13 @@ export default function Landing() {
           dangerouslySetInnerHTML={{ __html: PreBg }}></div>
           </div>
         </Link>
-        <div className="thumbnailBox h-full bg-[#171717] p-2 sm:p-3 md:p-4 lg:p-6  relative">
+        <div className="thumbnailBox h-full bg-[#171717] px-2 sm:px-3 md:px-4 lg:px-6 pb-0  relative">
           <div className="absolute top-5 left-5 right-0 font-GoshaSans text-xl text-[#fff]">sivarsa</div>
-          <div className="absolute top-16  md:top-4  lg:top-2 xl:top-5 right-2 sm:right-3 md:right-4 lg:right-5"  dangerouslySetInnerHTML={{ __html: messageUs }}>
+          <div className="messageusbtn absolute top-16  md:top-4  lg:top-2 xl:top-5 right-1 sm:right-3 md:right-4 lg:right-5"  dangerouslySetInnerHTML={{ __html: messageUs }}>
           </div>
 
           
-          <div className="flex absolute bottom-0 left-0 right-0 m-auto justify-center sivrsalogo overflow-hidden"  dangerouslySetInnerHTML={{ __html: sivarsaLogo }}></div>
+          <div className="flex justify-center items-end m-auto justify-center sivrsalogo overflow-hidden"  dangerouslySetInnerHTML={{ __html: sivarsaLogo }}></div>
       
         </div>
         <Link to="/mobile-web-devlopment" className="relative thumbnailBox h-full bg-[#171717] p-2 sm:p-3 md:p-4 lg:p-6 ">
@@ -120,5 +392,10 @@ export default function Landing() {
           dangerouslySetInnerHTML={{ __html: nextBg }}></div>
         </Link>
     </div>
+
+
+
+
+
   );
 }
--- src/mobile/MobilePage1.js
+++ src/mobile/MobilePage1.js
@@ -25,8 +25,8 @@ export default function MobilePage1() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left p-2 sm:p-3 md:p-4 lg:p-6 bg-[#6FF04A]">       
-              <h2 className="font-neuebit  text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl text-[#0B6222] mb-5">Web Development </h2>
-              <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl text-[#0B6222]">Take your business online <br /> with a premium website</p>
+              <h2 className="font-neuebit  text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#0B6222] mb-5">Web Development </h2>
+              <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#0B6222]">Take your business online <br /> with a premium website</p>
              <div
           className="absolute absolute  bottom-2 xm:bottom-4 md:bottom-5 lg:bottom-7 xl:bottom-8 right-2 sm:right-4 md:right-5 lg:right-7 xl:right-8"
           dangerouslySetInnerHTML={{ __html: arrowblackUP }}
@@ -46,8 +46,8 @@ export default function MobilePage1() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left p-2 sm:p-3 md:p-4 lg:p-6 bg-[#FF7E2D]">       
-              <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl text-[#9D3001]  mb-5">AI Development </h2>
-              <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl text-[#9D3001]">Unleash the power of AI to <br /> transform your business</p>
+              <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#9D3001]  mb-5">AI Development </h2>
+              <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#9D3001]">Unleash the power of AI to <br /> transform your business</p>
               <div className="absolute absolute  bottom-2 xm:bottom-4 md:bottom-5 lg:bottom-7 xl:bottom-8 right-2 sm:right-4 md:right-5 lg:right-7 xl:right-8" dangerouslySetInnerHTML={{ __html: arrowblackUP }}></div>
               </div>
               <div className="wall text-white right"></div>
@@ -61,7 +61,7 @@ export default function MobilePage1() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left p-2 sm:p-3 md:p-4 lg:p-6 bg-[#393939]">       
-               <h2 className="font-GoshaSans text-xm  md:text-2xl lg:text-4xl xl:text-5xl text-white "> Services <br></br> Provided </h2>
+               <h2 className="font-neuebit leading-normal text-xm  sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl leading-none text-white "> Services <br></br> Provided </h2>
               </div>
               <div className="wall text-white right"></div>
               <div className="wall text-white top"></div>
@@ -76,8 +76,8 @@ export default function MobilePage1() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left  p-2 sm:p-3 md:p-4 lg:p-6 bg-[#F559C4]">       
-              <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl text-[#F3C7E7] mb-5">Backend Services </h2>
-        <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl text-[#F3C7E7]">Creating robust and scalable APIs to connect your services.
+              <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#F3C7E7]  mb-5">Backend Services </h2>
+        <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#F3C7E7]">Creating robust and scalable APIs to connect your services.
         </p>
         <div
           className="absolute absolute  bottom-2 xm:bottom-4 md:bottom-5 lg:bottom-7 xl:bottom-8 right-2 sm:right-4 md:right-5 lg:right-7 xl:right-8"
@@ -99,8 +99,8 @@ export default function MobilePage1() {
               <div className="wall text-white front"></div>
               <div className="wall text-white back"></div>
               <div className="wall text-white left p-2 sm:p-3 md:p-4 lg:p-6 bg-[#763DF8]">       
-              <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl text-[#CFB8E5] mb-5">UI & UX Design</h2>
-               <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl text-[#CFB8E5]">Ignite your brand’s potential <br/> with stunning, innovative <br/> design.</p>
+              <h2 className="font-neuebit text-base sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl leading-none text-[#CFB8E5] mb-5">UI & UX Design</h2>
+               <p className="font-GoshaSans  text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl leading-none text-[#CFB8E5]">Ignite your brand’s potential <br/> with stunning, innovative <br/> design.</p>
                 <div className="absolute  bottom-2 xm:bottom-4 md:bottom-5 lg:bottom-7 xl:bottom-8 right-2 sm:right-4 md:right-5 lg:right-7 xl:right-8" dangerouslySetInnerHTML={{ __html: arrowblackDW }}></div>
               </div>
               <div className="wall text-white right"></div>
@@ -130,7 +130,7 @@ export default function MobilePage1() {
         </Link>
         <div className="thumbnailBox h-full bg-[#171717] p-2 sm:p-3 md:p-4 lg:p-6  relative">
           <div className="absolute top-5 left-5 right-0 font-GoshaSans text-xl text-[#fff]">sivarsa</div>
-          <div className="absolute top-16  md:top-4  lg:top-2 xl:top-5 right-2 sm:right-3 md:right-4 lg:right-5"  dangerouslySetInnerHTML={{ __html: messageUs }}>
+          <div className="messageusbtn absolute top-16  md:top-4  lg:top-2 xl:top-5 right-2 sm:right-3 md:right-4 lg:right-5"  dangerouslySetInnerHTML={{ __html: messageUs }}>
           </div>
 
           
