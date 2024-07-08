cf12bb563f1e1e01472d1564a9b45602234a1a88
Abhinav Mathur <mathur.abhinav1108@gmail.com>
2024-07-08 11:43:08 +0530

    Rotation added in test.js

--- src/Test.js
+++ src/Test.js
@@ -1,75 +1,24 @@
 import React, { useEffect } from 'react'
+import "../src/test.css"
 
 export default function Test() {
 
-   useEffect(() => {
-      const cubes = document.getElementsByClassName('cube');
-      Array.from(cubes).forEach(cube => {
-         cube.style.transition = 'transform 1s';  
-         cube.style.transform = 'rotateX(0deg) rotateY(90deg) rotateZ(0deg)';
-       });
-   }, []);
+  useEffect(() => {
+    const cube = document.querySelector('.cube');
+    setTimeout(() => {
+      cube.classList.add('rotate');
+    }, 1000); // Delay to ensure the cube is rendered before rotation starts
+  }, []);
 
   return (
-    <div className='bg-black min-h-screen flex justify-center items-center'>
-      <div>
-         <div className="flex">
-         <div class="flex">
-          <div class="cube">
-            <div class="wall text-white front">Front</div>
-            <div class="wall text-white back">Back</div>
-            <div class="wall text-white left">Left</div>
-            <div class="wall text-white right">Right</div>
-            <div class="wall text-white top">Top</div>
-            <div class="wall text-white bottom">Bottom</div>
-          </div>
-        </div>
-
-          <div className="cube">
-            <div className="wall text-white front"></div>
-            <div className="wall text-white back">Back</div>
-            <div className="wall text-white left">Left</div>
-            <div className="wall text-white right"></div>
-            <div className="wall text-white top">Top</div>
-            <div className="wall text-white bottom">Bottom</div>
-          </div>
-          <div className="cube">
-            <div className="wall text-white front">Front</div>
-            <div className="wall text-white back">Back</div>
-            <div className="wall text-white left">Left</div>
-            <div className="wall text-white right">Right</div>
-            <div className="wall text-white top">Top</div>
-            <div className="wall text-white bottom">Bottom</div>
-          </div>
-        </div>
-         <div className="flex">
-          <div className="cube">
-            <div className="wall text-white front"></div>
-            <div className="wall text-white back">Back</div>
-            <div className="wall text-white left">Left</div>
-            <div className="wall text-white right"></div>
-            <div className="wall text-white top">Top</div>
-            <div className="wall text-white bottom">Bottom</div>
-          </div>
-          <div className="cube">
-            <div className="wall text-white front">Front</div>
-            <div className="wall text-white back">Back</div>
-            <div className="wall text-white left">Left</div>
-            <div className="wall text-white right">Right</div>
-            <div className="wall text-white top">Top</div>
-            <div className="wall text-white bottom">Bottom</div>
-          </div>
-          <div className="cube">
-            <div className="wall text-white front"></div>
-            <div className="wall text-white back">Back</div>
-            <div className="wall text-white left">Left</div>
-            <div className="wall text-white right"></div>
-            <div className="wall text-white top">Top</div>
-            <div className="wall text-white bottom">Bottom</div>
-          </div>
-        </div>
-         
-
+    <div className="scene">
+      <div className="cube">
+        <div className="cube-face cube-face--front">Front</div>
+        <div className="cube-face cube-face--back">Back</div>
+        <div className="cube-face cube-face--left">Left</div>
+        <div className="cube-face cube-face--right">Right</div>
+        <div className="cube-face cube-face--top">Top</div>
+        <div className="cube-face cube-face--bottom">Bottom</div>
       </div>
     </div>
   )
